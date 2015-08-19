14d55543187b2426079e041e27a02d4aa8ed419f
hasclass <sebi.burkhard@gmail.com>
2015-08-19 10:27:55 +0700

    fix specs for jruby

--- spec/date_period_parser_spec.rb
+++ spec/date_period_parser_spec.rb
@@ -8,69 +8,69 @@ describe DatePeriodParser do
   end
 
   it '2014' do
-    assert_equal DateTime.parse("2014-01-01T00:00:00.000+0000"),  parse("2014").first
-    assert_equal DateTime.parse("2014-12-31T23:59:59.999+0000"),  parse("2014").last
+    assert_equal DateTime.new(2014, 1, 1,  0,  0,  0.000, "+0000"),  parse("2014").first
+    assert_equal DateTime.new(2014,12,31, 23, 59, 59.999, "+0000"),  parse("2014").last
   end
 
   it '2014-01' do
-    assert_equal DateTime.parse("2014-01-01T00:00:00.000+0000"),  parse("2014-01").first
-    assert_equal DateTime.parse("2014-01-31T23:59:59.999+0000"),  parse("2014-01").last
+    assert_equal DateTime.new(2014, 1, 1,  0,  0,  0.000, "+0000"),  parse("2014-01").first
+    assert_equal DateTime.new(2014, 1,31, 23, 59, 59.999, "+0000"),  parse("2014-01").last
   end
 
   it '2014-02' do
-    assert_equal DateTime.parse("2014-02-01T00:00:00.000+0000"),  parse("2014-02").first
-    assert_equal DateTime.parse("2014-02-28T23:59:59.999+0000"),  parse("2014-02").last
+    assert_equal DateTime.new(2014, 2, 1,  0,  0,  0.000, "+0000"),  parse("2014-02").first
+    assert_equal DateTime.new(2014, 2,28, 23, 59, 59.999, "+0000"),  parse("2014-02").last
 
-    assert_equal DateTime.parse("2012-02-01T00:00:00.000+0000"),  parse("2012-02").first
-    assert_equal DateTime.parse("2012-02-29T23:59:59.999+0000"),  parse("2012-02").last
+    assert_equal DateTime.new(2012, 2, 1,  0,  0,  0.000, "+0000"),  parse("2012-02").first
+    assert_equal DateTime.new(2012, 2,29, 23, 59, 59.999, "+0000"),  parse("2012-02").last
   end
 
   it '2014-12' do
-    assert_equal DateTime.parse("2014-12-01T00:00:00.000+0000"),  parse("2014-12").first
-    assert_equal DateTime.parse("2014-12-31T23:59:59.999+0000"),  parse("2014-12").last
+    assert_equal DateTime.new(2014,12, 1,  0,  0,  0.000, "+0000"),  parse("2014-12").first
+    assert_equal DateTime.new(2014,12,31, 23, 59, 59.999, "+0000"),  parse("2014-12").last
   end
 
   it '2014-08' do
-    assert_equal DateTime.parse("2014-08-01T00:00:00.000+0000"),  parse("2014-08").first
-    assert_equal DateTime.parse("2014-08-31T23:59:59.999+0000"),  parse("2014-08").last
+    assert_equal DateTime.new(2014, 8, 1,  0,  0,  0.000, "+0000"),  parse("2014-08").first
+    assert_equal DateTime.new(2014, 8,31, 23, 59, 59.999, "+0000"),  parse("2014-08").last
   end
 
   it '2014-01-01' do
-    assert_equal DateTime.parse("2014-01-01T00:00:00.000+0000"),  parse("2014-01-01").first
-    assert_equal DateTime.parse("2014-01-01T23:59:59.999+0000"),  parse("2014-01-01").last
+    assert_equal DateTime.new(2014, 1, 1,  0,  0,  0.000, "+0000"),  parse("2014-01-01").first
+    assert_equal DateTime.new(2014, 1, 1, 23, 59, 59.999, "+0000"),  parse("2014-01-01").last
   end
 
   it '2014-12-31' do
-    assert_equal DateTime.parse("2014-12-31T00:00:00.000+0000"),  parse("2014-12-31").first
-    assert_equal DateTime.parse("2014-12-31T23:59:59.999+0000"),  parse("2014-12-31").last
+    assert_equal DateTime.new(2014,12,31,  0,  0,  0.000, "+0000"),  parse("2014-12-31").first
+    assert_equal DateTime.new(2014,12,31, 23, 59, 59.999, "+0000"),  parse("2014-12-31").last
   end
 
   it 'today' do
-    today = Date.today
-    assert_equal DateTime.parse("#{today}T00:00:00.000+0000"),  parse("today").first
-    assert_equal DateTime.parse("#{today}T23:59:59.999+0000"),  parse("today").last
+    t = Date.today
+    assert_equal DateTime.new(t.year, t.month, t.day, 0, 0, 0.000, "+0000"),  parse("today").first
+    assert_equal DateTime.new(t.year, t.month, t.day,23,59,59.999, "+0000"),  parse("today").last
   end
 
   describe "with offsets" do
     it '2014' do
-      assert_equal DateTime.parse("2014-01-01T00:00:00.000+0700"),  parse("2014", "+0700").first
-      assert_equal DateTime.parse("2014-12-31T23:59:59.999+0700"),  parse("2014", "+0700").last
+      assert_equal DateTime.new(2014, 1, 1,  0,  0,  0.000, "+0700"),  parse("2014", "+0700").first
+      assert_equal DateTime.new(2014,12,31, 23, 59, 59.999, "+0700"),  parse("2014", "+0700").last
     end
 
     it '2014-01' do
-      assert_equal DateTime.parse("2014-01-01T00:00:00.000+0700"),  parse("2014-01", "+7").first
-      assert_equal DateTime.parse("2014-01-31T23:59:59.999+0700"),  parse("2014-01", "+7").last
+      assert_equal DateTime.new(2014, 1, 1,  0,  0,  0.000, "+0700"),  parse("2014-01", "+7").first
+      assert_equal DateTime.new(2014, 1,31, 23, 59, 59.999, "+0700"),  parse("2014-01", "+7").last
     end
 
     it '2014-01-01' do
-      assert_equal DateTime.parse("2014-01-01T00:00:00.000+0700"),  parse("2014-01-01", "+7").first
-      assert_equal DateTime.parse("2014-01-01T23:59:59.999+0700"),  parse("2014-01-01", "+7").last
+      assert_equal DateTime.new(2014, 1, 1,  0,  0,  0.000, "+0700"),  parse("2014-01-01", "+7").first
+      assert_equal DateTime.new(2014, 1, 1, 23, 59, 59.999, "+0700"),  parse("2014-01-01", "+7").last
     end
 
     it 'today' do
-      today = Date.today
-      assert_equal DateTime.parse("#{today}T00:00:00.000+0700"),  parse("today", "+7").first
-      assert_equal DateTime.parse("#{today}T23:59:59.999+0700"),  parse("today", "+7").last
+      t = Date.today
+      assert_equal DateTime.new(t.year, t.month, t.day,  0,  0,  0.000, "+0700"),  parse("today", "+7").first
+      assert_equal DateTime.new(t.year, t.month, t.day, 23, 59, 59.999, "+0700"),  parse("today", "+7").last
     end
   end
 
